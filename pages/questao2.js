3a6e74267f1b3fb48a10f8cb9f8ae3e2aae2a3ad
PedroAugustoMD <77705353+PedroAugustoMD@users.noreply.github.com>
2022-01-18 14:36:45 -0300

    Add files via upload

---
+++ pages/questao2.js
@@ -0,0 +1,10 @@
+import {HinoDoVasco} from "./funcao.js"
+
+export default function Home(){
+    return (
+      <div>
+        <HinoDoVasco />
+    
+      </div>
+    )
+  }
---
+++ pages/sl.js
@@ -0,0 +1,35 @@
+export default function Home(){
+
+    return (
+ 
+       <div>
+ 
+          <h2>
+ 
+            Quem é Neymar?<br />
+            Para o cego, é a luz.<br />
+            Para o faminto, é o pão.<br />
+            Para o sedento, é a fonte de água.<br />
+            Para o morto, é a vida.<br />
+            Para o enfermo, é a cura.<br />
+            Para o prisioneiro, é a liberdade.<br />
+            Para o solitário, é o companheiro.<br />
+            Para o viajante, é o caminho.<br />
+            Para mim, é tudo.<br />
+            Se o Neymar acha, eu concordo.<br />
+            Se o Neymar fala, eu escuto.<br />
+            Se o Neymar erra, eu perdoo.<br />
+            Se o Neymar pensa, eu admiro.<br />
+            Se o Neymar tem 100 fãs, eu sou um deles.<br />
+            Se o Neymar tem 10 fãs, eu sou um deles.<br />
+            Se o Neymar tem 1 fã, eu sou esse fã.<br />
+            Se o Neymar não tem fãs, eu não existo.<br />
+            <img src="https://pbs.twimg.com/media/EfOqvaeX0AAZ0Gh.jpg"></img>
+ 
+          </h2>
+ 
+       </div>
+ 
+    )
+ 
+ }
